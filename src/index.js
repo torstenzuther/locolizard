e6401c61281d982735bfb737abe5ddadf9dd569d
buddelflink87 <torsten.zuther@web.de>
2020-03-23 22:28:07 +0100

    extended onObserved

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
 import { visit } from './visit';
 
-const callback = mutationList => {
+const onObserved = mutationList => {
   mutationList.forEach(mutation => {
       if (mutation.addedNodes) {
           for (let i=0; i<mutation.addedNodes.length; i++) {
               visit(mutation.addedNodes[i]);
           }
       }
+      if (mutation.target) {
+        visit(mutation.target);
+      }
   });
 };
 
 const onDomLoaded = () => {
   console.info('Locolizard intialized');
-  var observer = new MutationObserver(callback);
+  var observer = new MutationObserver(onObserved);
   var observerOptions = {
       childList: true,
-      // characterData: true,
-      // attributes: true,
+      characterData: true,
+      attributes: true,
       subtree: true
   };
   observer.observe(document.body, observerOptions);
